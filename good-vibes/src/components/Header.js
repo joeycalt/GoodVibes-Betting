53e1b92356d3456331035b8d3e31482bc7aceed4
joeycalt <joeycalt@yahoo.com>
2023-06-06 12:59:36 -0400

    edited nav bar

--- good-vibes/src/components/Header.js
+++ good-vibes/src/components/Header.js
@@ -11,11 +11,11 @@ const Header = () => {
     <Navbar.Toggle aria-controls='responsive-navbar-nav' data-bs-toggle="collapse"/>
     <Navbar.Collapse id='responsive-navbar-nav'>
       <Nav>
-        <Nav.Link className='navbar-links' href='/home'>Home</Nav.Link>
-        <Nav.Link href='/baseball'>Baseball</Nav.Link>
-        <Nav.Link href='/hockey'>Hockey</Nav.Link>
-        <Nav.Link href='/football'>Football</Nav.Link>
-        <Nav.Link href='/about'>About</Nav.Link>
+        <Nav.Link className='navbar-links' to='/home'>Home</Nav.Link>
+        <Nav.Link to='/baseball'>Baseball</Nav.Link>
+        <Nav.Link to='/hockey'>Hockey</Nav.Link>
+        <Nav.Link to='/football'>Football</Nav.Link>
+        <Nav.Link to='/about'>About</Nav.Link>
       </Nav>
     </Navbar.Collapse>
   </Container>
